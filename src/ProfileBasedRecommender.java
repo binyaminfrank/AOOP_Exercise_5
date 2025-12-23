6d42e2c5585e85ed83f50eda3d9422447d88ebe4
Abraham (Avi) Soclof <abrahams@wix.com>
2025-12-23 07:09:00 +0200

    work in progress

--- src/ProfileBasedRecommender.java
+++ src/ProfileBasedRecommender.java
@@ -15,22 +15,54 @@ class ProfileBasedRecommender<T extends Item> extends RecommenderSystem<T> {
 
     @Override
     public List<T> recommendTop10(int userId) {
-        // TODO: implement
-        Map<Integer, List<Rating<T>>> itemRatings = ratings.stream().collect(Collectors.groupingBy(Rating::getItemId));
+        List<User> matchingUsers = getMatchingProfileUsers(userId);
 
+        // convert users -> userIds (so we can filter ratings easily)
+        Set<Integer> matchingUserIds = matchingUsers.stream()
+                .map(User::getId)
+                .collect(Collectors.toSet());
+
+        // 1) group ratings by itemId (only from matching users)
+        Map<Integer, List<Rating>> ratingsByItem = ratings.stream()
+                .filter(r -> matchingUserIds.contains(r.getUserId()))
+                .collect(Collectors.groupingBy(Rating::getItemId));
+
+        // 2) keep items with >= 5 ratings, compute avg
+        Map<Integer, Double> avgByItem = ratingsByItem.entrySet().stream()
+                .filter(e -> e.getValue().size() >= 5)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> Double.valueOf(
+                                e.getValue().stream()
+                                        .mapToDouble(Rating::getRating)
+                                        .average()
+                                        .orElse(0.0)
+                        )
+                ));
 
 
         return null;
     }
 
     public List<User> getMatchingProfileUsers(int userId) {
-        // TODO: implement
+        //
+        User currentUser = users.get(Integer.valueOf(userId));
+        String gender = currentUser.getGender();
+        int age = currentUser.getAge();
 
-
-
-
-
-
-        return null;
+        return  users.values().stream()
+                .filter(u -> u.getGender().equals(gender)) //same gender
+                .filter(u -> Math.abs(u.getAge() - age) <= 5)   // within 5 years
+                .filter(u -> u.getId() != currentUser.getId()) //excludes current user
+                .toList();
     }
+
+
+
+
+
 }
+
+
+
+
